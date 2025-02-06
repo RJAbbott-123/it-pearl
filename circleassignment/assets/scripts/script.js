7b5c442f91a3bc0206515ba43311e29b17787d5a
RJAbbott-123 <rjabbott@ualr.edu>
2025-02-06 20:07:58 +0000

    added javascript

--- circleassignment/assets/scripts/script.js
+++ circleassignment/assets/scripts/script.js
@@ -19,6 +19,9 @@ function CircleCalculations(){
     circumference = calculateCircumference(radiusfp);
     document.getElementById("circumference").innerHTML = circumference;
     let x = 1;
+
+    area= calculateArea(radiusfp)
+    document.getElementById("area").innerHTML = area;
     }
 }
 
@@ -29,4 +32,44 @@ function calculateDiameter(r) {
 function calculateCircumference(r) {
 
     return 2 * Math.PI * r;
+}
+
+function calculateArea(r) {
+    return 2 * Math.PI * r^2;
+}
+$("#CircleForm").validate({
+
+});
+
+function CircleCalculations(){
+    if($("#CircleForm").valid()) {
+    let radius;
+    let diameter;
+    let circumference;
+    let radiusfp;
+    let area;
+
+    radius = document.getElementById("radius").value;
+    radiusfp = parseFloat(radius)
+
+    diameter = calculateDiameter(radiusfp)
+    document.getElementById("diameter").innerHTML = diameter;
+
+    circumference = calculateCircumference(radiusfp);
+    document.getElementById("circumference").innerHTML = circumference;
+    let x = 1;
+
+    area= calculateArea(radiusfp)
+    document.getElementById("area").innerHTML = area;
+    }
+}
+function calculateDiameter(r) {
+    return 2 * r;
+}
+function calculateCircumference(r) {
+
+    return 2 * Math.PI * r;
+}
+function calculateArea(r) {
+    return Math.PI * Math.pow(r,2);
 }
