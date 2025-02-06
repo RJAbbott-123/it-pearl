c598cdf6175c4f5666eeb0df294d01258caaacb6
RJAbbott-123 <rjabbott@ualr.edu>
2025-02-06 20:35:54 +0000

    fixed javascript

--- circleassignment/assets/scripts/script.js
+++ circleassignment/assets/scripts/script.js
@@ -9,44 +9,6 @@ function CircleCalculations(){
     let diameter;
     let circumference;
     let radiusfp;
-
-    radius = document.getElementById("radius").value;
-    radiusfp = parseFloat(radius)
-
-    diameter = calculateDiameter(radiusfp)
-    document.getElementById("diameter").innerHTML = diameter;
-
-    circumference = calculateCircumference(radiusfp);
-    document.getElementById("circumference").innerHTML = circumference;
-    let x = 1;
-
-    area= calculateArea(radiusfp)
-    document.getElementById("area").innerHTML = area;
-    }
-}
-
-function calculateDiameter(r) {
-    return 2 * r;
-}
-
-function calculateCircumference(r) {
-
-    return 2 * Math.PI * r;
-}
-
-function calculateArea(r) {
-    return 2 * Math.PI * r^2;
-}
-$("#CircleForm").validate({
-
-});
-
-function CircleCalculations(){
-    if($("#CircleForm").valid()) {
-    let radius;
-    let diameter;
-    let circumference;
-    let radiusfp;
     let area;
 
     radius = document.getElementById("radius").value;
@@ -67,9 +29,16 @@ function calculateDiameter(r) {
     return 2 * r;
 }
 function calculateCircumference(r) {
-
     return 2 * Math.PI * r;
 }
 function calculateArea(r) {
     return Math.PI * Math.pow(r,2);
+}
+function clearForm()
+{
+    document.getElementById("radius").value = "";
+    document.getElementById("radiuserror").innerHTML = "";
+    document.getElementById("diameter").innerHTML = "";
+    document.getElementById("circumference").innerHTML = "";
+    document.getElementById("area").innerHTML = "";
 }
